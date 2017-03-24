e8cb9138197fca9967f8342277c929801abd4ad3
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-24 19:26:32 +0430

    Creating Bank Dao

---
+++ src/edu/sharif/ce/snapshot/core/model/dao/BankDao.java
@@ -0,0 +1,8 @@
+package edu.sharif.ce.snapshot.core.model.dao;
+
+
+/**
+ * The interface Bank data access object.
+ */
+public interface BankDao {
+}
---
+++ src/edu/sharif/ce/snapshot/core/model/dao/BankDaoImpl.java
@@ -0,0 +1,8 @@
+package edu.sharif.ce.snapshot.core.model.dao;
+
+
+/**
+ * The Bank data access object class.
+ */
+public class BankDaoImpl implements BankDao {
+}
