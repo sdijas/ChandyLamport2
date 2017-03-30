1e00bb8fb36464d4f0b999dac910224a34d11e85
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-30 23:40:10 +0430

    Adds Main.java file

---
+++ src/edu/sharif/ce/Main.java
@@ -0,0 +1,10 @@
+package edu.sharif.ce;
+
+import edu.sharif.ce.config.Configuration;
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Configuration.INITIAL_BANK_CREDIT.get());
+  }
+}
