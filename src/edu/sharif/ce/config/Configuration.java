70663c87ed976ad13e07104ba4afd599c11a2516
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-30 23:38:52 +0430

    Creating lambda expression to read property

--- src/edu/sharif/ce/config/Configuration.java
+++ src/edu/sharif/ce/config/Configuration.java
@@ -1,11 +1,22 @@
 package edu.sharif.ce.config;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * The Configuration class. This class holds global variables
  *
  * @author Alireza Aghamohammadi
  */
 public final class Configuration {
+  private static Properties properties;
+  private static final String CONFIGURATION_FILE = "config.properties";
+
+  /**
+   * The initial credit of bank.
+   */
+  public static final Setting<Integer> INITIAL_BANK_CREDIT = () -> Integer.parseInt(properties.getProperty("InitialBankCredit"));
+
 
   /**
    * make sure there is no instance of this class can create
@@ -13,11 +24,21 @@ public final class Configuration {
   private Configuration() {
   }
 
+  static {
+    properties = new Properties();
+    try {
+      properties.load(Configuration.class.getClassLoader().getResourceAsStream(CONFIGURATION_FILE)); // load config.properties file
+    } catch (IOException e) {
+      System.err.println("Loading configuration file " + CONFIGURATION_FILE + " failed");
+    }
+  }
+
   /**
    * The interface Setting.
    *
    * @param <T> the type parameter
    */
+  @FunctionalInterface
   public interface Setting<T> {
     /**
      * Get t.
