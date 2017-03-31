849a3df150535979fa60cba51e7fe387af47a024
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-31 12:59:15 +0430

    Implemting deposit/withdraw

--- src/edu/sharif/ce/snapshot/core/model/dao/BankDaoImpl.java
+++ src/edu/sharif/ce/snapshot/core/model/dao/BankDaoImpl.java
@@ -1,6 +1,9 @@
 package edu.sharif.ce.snapshot.core.model.dao;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.sharif.ce.snapshot.core.model.entity.Bank;
 
 /**
@@ -9,13 +12,20 @@ import edu.sharif.ce.snapshot.core.model.entity.Bank;
  * @author Alireza Aghamohammadi
  */
 public class BankDaoImpl implements BankDao {
+  /**
+   * The Banks.
+   */
+  List<Bank> banks = new ArrayList<>();
+
   @Override
   public void deposit(Bank bank) {
-    //TODO
+    Bank that = banks.get(bank.getId());
+    that.setBalance(that.getBalance() + bank.getBalance());
   }
 
   @Override
   public void withdraw(Bank bank) {
-    //TODO
+    Bank that = banks.get(bank.getId());
+    that.setBalance(that.getBalance() - bank.getBalance());
   }
 }
--- src/edu/sharif/ce/snapshot/core/model/entity/Bank.java
+++ src/edu/sharif/ce/snapshot/core/model/entity/Bank.java
@@ -8,17 +8,39 @@ package edu.sharif.ce.snapshot.core.model.entity;
  */
 public class Bank {
 
-
-  // The amount of money held in a bank account at a given moment.
+  // The amount of money held in a bank account at a given moment
   private int balance;
 
+  // The identifier bank account
+  private int id;
+
+
+  /**
+   * Sets balance.
+   *
+   * @param balance the balance
+   */
+  public void setBalance(int balance) {
+    this.balance = balance;
+  }
 
   /**
    * Gets balance.
    *
-   * @return the amount of money held in a bank account at a given moment.
+   * @return the amount of money held in a bank account at a given moment
    */
   public int getBalance() {
     return balance;
   }
+
+  /**
+   * Gets id.
+   *
+   * @return the identifier of bank
+   */
+  public int getId() {
+    return id;
+  }
+
+
 }
