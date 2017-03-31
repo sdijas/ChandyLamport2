94f672ea58fd3c4befc2d52674ac07bf6625aa01
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-31 12:11:42 +0430

    Adding deposit/withdraw signature

--- src/edu/sharif/ce/snapshot/core/model/dao/BankDao.java
+++ src/edu/sharif/ce/snapshot/core/model/dao/BankDao.java
@@ -3,6 +3,21 @@ package edu.sharif.ce.snapshot.core.model.dao;
 
 /**
  * The interface Bank data access object.
+ *
+ * @author Alireza Aghamohammadi
  */
 public interface BankDao {
+  /**
+   * Deposit.
+   *
+   * @param amount the amount
+   */
+  public void deposit(double amount);
+
+  /**
+   * Withdraw.
+   *
+   * @param amount the amount
+   */
+  public void withdraw(double amount);
 }
--- src/edu/sharif/ce/snapshot/core/model/dao/BankDaoImpl.java
+++ src/edu/sharif/ce/snapshot/core/model/dao/BankDaoImpl.java
@@ -3,6 +3,17 @@ package edu.sharif.ce.snapshot.core.model.dao;
 
 /**
  * The Bank data access object class.
+ *
+ * @author Alireza Aghamohammadi
  */
 public class BankDaoImpl implements BankDao {
+  @Override
+  public void deposit(double amount) {
+    //TODO
+  }
+
+  @Override
+  public void withdraw(double amount) {
+    //TODO
+  }
 }
